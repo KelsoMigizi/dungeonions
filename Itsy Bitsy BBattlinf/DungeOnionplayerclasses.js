2149d170b111e3c4fcf050b1a7a9f428cc376b6e
KelsoMigizi <pwndabombclan@gmail.com>
2025-10-03 08:07:54 -0700

    Tighten camera alignment and relocate HUD

--- Itsy Bitsy BBattlinf/DungeOnionplayerclasses.js
+++ Itsy Bitsy BBattlinf/DungeOnionplayerclasses.js
@@ -1,13 +1,26 @@
 const playerClasses = {
     wizard: {
         name: 'Wizard',
+        description: 'A nimble spellcaster who hurls volatile potions at breakneck speed.',
         tileIndex: 84,
         baseHealth: 100,
         baseSpeed: 120, // pixels per second
         projectileDamage: 5,
         projectileSpeed: 250, // pixels per second
         projectileFireRate: 200, // ms
-    }
+        projectileTileIndex: 115,
+    },
+    paladin: {
+        name: 'Paladin',
+        description: 'An armored guardian chosen from the hero tiles who trades speed for survivability and righteous power.',
+        tileIndex: 72,
+        baseHealth: 150,
+        baseSpeed: 90,
+        projectileDamage: 8,
+        projectileSpeed: 220,
+        projectileFireRate: 320,
+        projectileTileIndex: 117,
+    },
     // Future classes like Knight, Archer, etc. can be added here
 };
 
@@ -19,6 +32,7 @@ class Player {
         }
 
         this.classInfo = playerClass;
+        this.className = playerClassName;
         
         // Position and core stats
         this.x = 256;
@@ -32,6 +46,7 @@ class Player {
         this.projectileDamage = this.classInfo.projectileDamage;
         this.projectileSpeed = this.classInfo.projectileSpeed;
         this.projectileFireRate = this.classInfo.projectileFireRate;
+        this.projectileTileIndex = this.classInfo.projectileTileIndex || 115;
     }
 
     takeDamage(amount) {
@@ -47,4 +62,9 @@ class Player {
         this.y = 160;
         this.health = this.maxHealth;
     }
+}
+
+if (typeof window !== 'undefined') {
+    window.playerClasses = playerClasses;
+    window.Player = Player;
 }
